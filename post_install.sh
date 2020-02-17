91c620c9d55d3c8053ff850f41d4bdd39d084b9b
Michael Englehorn <michael@englehorn.com>
2020-02-16 18:49:59 -0600

    Need to initialize the database on first boot

--- post_install.sh
+++ post_install.sh
@@ -3,6 +3,9 @@
 # Enable the service
 sysrc -f /etc/rc.conf postgresql_enable="YES"
 
+# Initialize database
+service postgresql initdb 2>/dev/null
+
 # Start the service
 service postgresql start 2>/dev/null
 
